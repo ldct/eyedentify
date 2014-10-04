8ce923e5f14a3d3c8f9f2d288dc81e145c8e367a
Li Xuanji <xuanji@gmail.com>
2014-10-05 05:45:53 +0800

    add cors

--- hello.py
+++ hello.py
@@ -1,10 +1,22 @@
-from bottle import route, run, response
+import bottle
+from bottle import response, request, hook
 
-@route('/')
-def hello():
+import base64
+
+@bottle.hook('after_request')
+def enable_cors():
+    response.headers['Access-Control-Allow-Origin'] = '*'
+
+@bottle.get('/')
+def health():
   response.content_type = 'application/json'
   return {'status': 'ok'}
+
+@bottle.post('/analyse')
+def analyse():
+  image_base64 = request.POST.get('image')
+
 try:
-  run(host='0.0.0.0', port=80, debug=False)
+  bottle.run(host='0.0.0.0', port=80, debug=False)
 except:
-  run(host='localhost', port=8080, debug=True)
+  bottle.run(host='localhost', port=8080, debug=True)
