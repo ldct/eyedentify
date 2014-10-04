cb0c6658aa6f56555e4f8294ac06cfce4628b7a9
Li Xuanji <xuanji@gmail.com>
2014-10-05 06:24:28 +0800

    stuff

--- hello.py
+++ hello.py
@@ -1,21 +1,33 @@
 import bottle
-from bottle import response, request, hook
+from bottle import response, request, hook, route
 
 import base64
 
 @bottle.hook('after_request')
 def enable_cors():
     response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
 
 @bottle.get('/')
 def health():
   response.content_type = 'application/json'
   return {'status': 'ok'}
 
+@bottle.route('/analyse', method=['OPTIONS'])
+def analyse_options():
+  return None
+
 @bottle.post('/analyse')
 def analyse():
-  image_base64 = request.POST.get('image')
-  return {'status': 'ok', 'overlay': image_base64[0:10]}
+  try:
+    #image_base64 = request.POST.get('image')
+
+    print request.files.get('image')
+    return {'status': 'ok'}
+
+  except:
+    return {'status': 'oops'}
 
 try:
   bottle.run(host='0.0.0.0', port=80, debug=False)
