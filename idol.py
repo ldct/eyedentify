cfc617ff60c38a3bad5e1a8daa67034e3fea6702
Li Xuanji <xuanji@gmail.com>
2014-10-05 07:50:03 +0800

    ocr

--- idol.py
+++ idol.py
@@ -1,13 +1,14 @@
 import requests
-url="https://api.idolondemand.com/1/api/sync/ocrdocument/v1"
+url="http://api.idolondemand.com/1/api/sync/{}/v1"
 apikey="8e1741c1-1c77-4be0-b0eb-5084924de171"
 
 def postrequests(function,data={},files={}):
-   data["8e1741c1-1c77-4be0-b0eb-5084924de171"]=apikey
-   callurl=url.format(function)
-   r=requests.post(callurl,data=data,files=files)
-   return r.json()
+    data["apikey"]=apikey
+    callurl=url.format(function)
+    r=requests.post(callurl,data=data,files=files)
+    return r.json()
 
-results=postrequests('querytextindex',{'text':'great'})
-for document in results["documents"]:
-   print document["title"]
+f = open('text.jpg', 'rb')
+
+results = postrequests('ocrdocument', files= {'file': f}, data={'mode': 'scene_photo'})
+print results
