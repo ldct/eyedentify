5cb2fbd357c92ef158682aaaf4545bca8180c4ce
Li Xuanji <xuanji@gmail.com>
2014-10-05 03:59:13 +0800

    json response

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
-from bottle import route, run
+from bottle import route, run, response
 
-@route('/hello')
+@route('/')
 def hello():
-    return "Hello World!"
-
-run(host='0.0.0.0', port=80, debug=False)
+  response.content_type = 'application/json'
+  return {'status': 'ok'}
+try:
+  run(host='0.0.0.0', port=80, debug=False)
+except:
+  run(host='localhost', port=8080, debug=True)
